595ca7337d6e6e947808a71702c329244aa2c37e
Matt Morris <mcm0027@yahoo.com>
2016-03-21 11:38:03 -0500

    add Forum store to initialize add and update forum data

---
+++ scripts/stores/ForumStore.js
@@ -0,0 +1,36 @@
+
+var answerData = {
+    "1": {
+        body: "Isn't that about time travel?",
+        correct: false
+    },
+    "2":{
+        body: "React and Flux are so nice",
+        correct: false
+    },
+    "3": {
+        body: "React is a synonym for 'respond'",
+        correct: false
+    }
+};
+
+var ForumStore = new EventEmitter();
+
+ForumStore.getAnswers = function() {
+    return answerData;
+};
+
+ForumStore.addAnswer = function(newAnswer) {
+    answerData[Object.keys(answerData).length + 1] = {
+        body: newAnswer,
+        correct: false
+    };
+};
+
+ForumStore.markAsCorrect = function(id) {
+    for (key in answerData) {
+        answerData[key].correct = false;
+    }
+
+    answerData[id].correct = true;
+}
